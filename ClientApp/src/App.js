b18091884026130daa19caeac05df66b9858bf76
Hoolanla <prakasit.y@gmail.com>
2019-07-06 07:39:39 +0700

    first commit

---
+++ ClientApp/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import Menu from './components/menu/menu';
+import Footer from './components/footer/footer';
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+ <div>
+<Header></Header>
+<Menu></Menu>
+<Footer></Footer>
+
+
+</div>
+
+
+    );
+  }
+}
+
+export default App;
